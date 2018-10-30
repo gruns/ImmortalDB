4f1727e825c7d938af2617ee59d4f5865eca43bf
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2018-10-30 16:50:32 -0500

    Improve TypeScript typings

--- iron-db.d.ts
+++ iron-db.d.ts
@@ -1,19 +1,43 @@
 //type definitions for iron-db
 
-export const DEFAULT_STORES: CookieStore[];
-export const DEFAULT_KEY_PREFIX: string;
-export class CookieStore {
-    constructor (ttl?:number);
-    get (key:string): Promise<string>;
-    set (key: string, value: string): Promise<void>;
-    remove (key: string): Promise<void>;
+export interface Store {
+    get(key: string): Promise<string | undefined>;
+    set(key: string, value: string): Promise<void>;
+    remove(key: string): Promise<void>;
 }
 
-export class IronStorage {
-    constructor (stores?:CookieStore[]);
+export const DEFAULT_STORES: Store[];
+export const DEFAULT_KEY_PREFIX: string;
+export class CookieStore implements Store {
+    constructor(ttl?: number);
 
-    get (key: string, _default?: string | null): Promise<string>;
-    set (key: string, value: string): Promise<String>;
+    get(key: string): Promise<string | undefined>;
+    set(key: string, value: string): Promise<void>;
+    remove(key: string): Promise<void>;
+}
+export class IndexedDbStore implements Store {
+    constructor(dbName?: string, storeName?: string);
+
+    get(key: string): Promise<string | undefined>;
+    set(key: string, value: string): Promise<void>;
+    remove(key: string): Promise<void>;
+}
+class StorageApiWrapper implements Store {
+    constructor(store: Storage);
+
+    get(key: string): Promise<string | undefined>;
+    set(key: string, value: string): Promise<void>;
+    remove(key: string): Promise<void>;
+}
+export class LocalStorageStore implements StorageApiWrapper {}
+export class SessionStorageStore implements StorageApiWrapper {}
+
+export class IronStorage {
+    constructor(stores?: Store[]);
+
+    get(key: string, _default?: null): Promise<string | null>;
+    get(key: string, _default: string): Promise<string>;
+    set(key: string, value: string): Promise<string>;
     remove(key: string): Promise<void>;
 }
 
