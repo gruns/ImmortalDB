875dc4eee63bd1e271145a6b055c317c0ac804d0
Ansgar Grunseid <grunseid@gmail.com>
2018-11-24 19:04:05 -0800

    Conform to JS Standard in the testing/ code.

--- testing/test.js
+++ testing/test.js
@@ -17,49 +17,49 @@ const PREFIX = ImmortalDB.DEFAULT_KEY_PREFIX
 
 
 function sleep (ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms))
 }
 
 
 ;(async () => {
-    const ele = (id) => document.getElementById(id)
+  const ele = (id) => document.getElementById(id)
 
-    const $key = ele('key')
-    const $value = ele('value')
+  const $key = ele('key')
+  const $value = ele('value')
 
-    ele('get').addEventListener(
-        'click', async () => $value.value = await db.get($key.value), false)
-    ele('set').addEventListener(
-        'click', async () => await db.set($key.value, $value.value), false)
-    ele('remove').addEventListener('click', async () => {
-        await db.remove($key.value)
-        $value.value = ''
-    }, false)
+  ele('get').addEventListener(
+    'click', async () => $value.value = await db.get($key.value), false)
+  ele('set').addEventListener(
+    'click', async () => await db.set($key.value, $value.value), false)
+  ele('remove').addEventListener('click', async () => {
+    await db.remove($key.value)
+    $value.value = ''
+  }, false)
 
-    ele('removeCookie').addEventListener(
-        'click', () => Cookies.remove(PREFIX + $key.value))
-    ele('removeIndexedDB').addEventListener(
-        'click', async () => await idbKeyval.del(PREFIX + $key.value, idb))
-    ele('removeLocalStorage').addEventListener(
-        'click', () => localStorage.removeItem(PREFIX + $key.value))
-    ele('removeSessionStorage').addEventListener(
-        'click', () => sessionStorage.removeItem(PREFIX + $key.value))
+  ele('removeCookie').addEventListener(
+    'click', () => Cookies.remove(PREFIX + $key.value))
+  ele('removeIndexedDB').addEventListener(
+    'click', async () => await idbKeyval.del(PREFIX + $key.value, idb))
+  ele('removeLocalStorage').addEventListener(
+    'click', () => localStorage.removeItem(PREFIX + $key.value))
+  ele('removeSessionStorage').addEventListener(
+    'click', () => sessionStorage.removeItem(PREFIX + $key.value))
 
-    ;(async function pollKeyLoop () {
-        const $cookies = ele('cookies')
-        const $indexedDB = ele('indexedDB')
-        const $localStorage = ele('localStorage')
-        const $sessionStorage = ele('sessionStorage')
+  ;(async function pollKeyLoop () {
+    const $cookies = ele('cookies')
+    const $indexedDB = ele('indexedDB')
+    const $localStorage = ele('localStorage')
+    const $sessionStorage = ele('sessionStorage')
 
-        while (true) {
-            const key = PREFIX + $key.value
+    while (true) {
+      const key = PREFIX + $key.value
 
-            $cookies.value = Cookies.get(key)
-            $indexedDB.value = await idbKeyval.get(key, idb)
-            $localStorage.value = localStorage.getItem(key)
-            $sessionStorage.value = sessionStorage.getItem(key)
+      $cookies.value = Cookies.get(key)
+      $indexedDB.value = await idbKeyval.get(key, idb)
+      $localStorage.value = localStorage.getItem(key)
+      $sessionStorage.value = sessionStorage.getItem(key)
 
-            await sleep(POLL_TIMEOUT)
-        }
-    })()
+      await sleep(POLL_TIMEOUT)
+    }
+  })()
 })()
