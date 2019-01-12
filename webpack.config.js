170f750b830bf98ca2193370515f1cfededc00f1
Ansgar Grunseid <grunseid@gmail.com>
2019-01-12 14:31:33 -0800

    Add trailing slash to path to connote a directory.

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = (env, argv) => ({
     output: {
         library: 'ImmortalDB',
         filename: '[name].js',
-        path: abspath('./dist'),
+        path: abspath('./dist/'),
     },
     optimization: {
         minimizer: [
