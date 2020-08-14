bc32c66d4ff2c6f94db26656e7b65a01af7c1a48
Ansgar Grunseid <grunseid@gmail.com>
2020-08-13 22:46:59 -0700

    Automatically open the test URL.

--- testing/cross-origin-iframe/runme.sh
+++ testing/cross-origin-iframe/runme.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-# To test cross origin storage, execute this file and navigate to
-#
-#   http://127.0.0.1:9090/testing/cross-origin-iframe/parent.html
-#
-# in your browser.
+# To test cross origin storage, execute this file.
 
 PROJECT_ROOT="../../"
 cd "$PROJECT_ROOT"
 
 python3 -m http.server 9090 &
-python3 -m http.server 9091
+pid=$!
+python3 -m http.server 9091 &
+python3 -m webbrowser \
+    -t "http://127.0.0.1:9090/testing/cross-origin-iframe/parent.html" &
+
+wait $pid
