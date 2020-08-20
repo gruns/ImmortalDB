71f535e939b498533e6256168b6cf84425c56ede
Ansgar Grunseid <grunseid@gmail.com>
2020-08-20 10:10:43 -0700

    Rename amICrossOriginIframe() to the clearer amIInsideACrossOriginIframe(). Add comments explaining the cross-origin iframe check.

--- src/cookie-store.js
+++ src/cookie-store.js
@@ -16,13 +16,19 @@ const DEFAULT_COOKIE_TTL = 365 // Days.
 // https://web.dev/samesite-cookies-explained/ and
 // https://tools.ietf.org/html/draft-west-cookie-incrementalism-00 for
 // details on SameSite and cross-origin behavior.
-const CROSS_ORIGIN_IFRAME = amICrossOriginIframe()
+const CROSS_ORIGIN_IFRAME = amIInsideACrossOriginIframe()
 const DEFAULT_SECURE = (CROSS_ORIGIN_IFRAME ? true : false)
 const DEFAULT_SAMESITE = (CROSS_ORIGIN_IFRAME ? 'None' : 'Lax')
 
 
-function amICrossOriginIframe () {
+function amIInsideACrossOriginIframe () {
     try {
+        // Raises ReferenceError if window isn't defined, eg if executed
+        // outside a browser.
+        //
+        // If inside a cross-origin iframe, raises: Uncaught
+        // DOMException: Blocked a frame with origin "..." from
+        // accessing a cross-origin frame.
         return !Boolean(window.top.location.href)
     } catch (err) {
         return true
