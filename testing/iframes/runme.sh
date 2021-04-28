f3a373a261b85804365827b7e8a97b8aeb4b35cc
Ansgar Grunseid <grunseid@gmail.com>
2021-04-27 22:20:03 -0700

    update the https testing to generate and use clearer certificate names: cert.pem to self-signed-dev-cert.pem and key.pem to self-signed-dev-key.pem

--- testing/iframes/runme.sh
+++ testing/iframes/runme.sh
@@ -4,18 +4,18 @@
 #
 # To test HTTPS, create development certs with
 #
-#   openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout key.pem -out cert.pem
+#   openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout self-signed-dev-key.pem -out self-signed-dev-cert.pem
 #
 # and run an HTTP server, like http-server, that supports HTTPS.
 
 PROJECT_ROOT="../../"
 cd "$PROJECT_ROOT"
 
-python3 -m http.server 9090 &
-#http-server -p 9090 -S &
+#python3 -m http.server 9090 &
+http-server -p 9090 -S -C self-signed-dev-cert.pem -K self-signed-dev-key.pem &
 pid=$!
-python3 -m http.server 9091 &
-#http-server -p 9091 -S &
+#python3 -m http.server 9091 &
+http-server -p 9091 -S -C self-signed-dev-cert.pem -K self-signed-dev-key.pem &
 
 python3 -m webbrowser \
     -t "http://127.0.0.1:9090/testing/iframes/same-origin.html" &
